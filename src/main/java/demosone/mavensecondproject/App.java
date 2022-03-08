ea04003f5bf1730ca60035263aa0695532846ffc
pandu790 <sruthibdu77@gmail.com>
2022-03-08 12:20:25 +0530

    i am commiting text file for some requirement

---
+++ src/main/java/demosone/mavensecondproject/App.java
@@ -0,0 +1,7 @@
+package demosone.mavensecondproject;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
---
+++ src/test/java/demosone/mavensecondproject/Demo.java
@@ -0,0 +1,12 @@
+package demosone.mavensecondproject;
+
+import org.testng.annotations.Test;
+
+public class Demo {
+	@Test
+	public void hellow() {
+		System.out.println("hellow()");
+		
+	}
+
+}
